b356f82eb770af18b17c35c6b74eebb81f3465e2
Cheffy <cheffy045@gmail.com>
2026-03-20 19:16:56 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,8 +2,8 @@ namespace RoomInfoMod
 {
     internal static class PluginInfo
     {
-        internal const string PLUGIN_GUID = "com.cheffy.streamercodemod";
-        internal const string PLUGIN_NAME = "StreamerCodeMOD";
+        internal const string PLUGIN_GUID = "com.cheffy.streamcodemod";
+        internal const string PLUGIN_NAME = "StreamCodeMOD";
         internal const string PLUGIN_VERSION = "1.0.0";
     }
 }
